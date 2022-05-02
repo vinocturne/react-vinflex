e62c4ebac537b4682834a15952cdbe5ec5257ef0
Ghunhee Shin <hago@rocketstaff.com>
2022-05-02 18:15:04 +0900

    Feat(Slider) : "추가 슬라이더"
    각 영화를 슬라이드 형식으로 펼칠 수 있도록 framer-motion을 활용하여 슬라이더를 제작하였습니다.
    현재 따로 버튼은 없는 상태로 메인 영화를 클릭할 시 페이지가 전환됩니다.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IMoviesResult } from "../api";
@@ -36,12 +37,61 @@ const Overview = styled.p`
     width: 50%;
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top: -100px;
+`;
+
+const Row = styled(motion.div)`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px;
+    margin-bottom: 5px;
+    position: absolute;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+    /* background-color: white; */
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center;
+    height: 200px;
+    color: red;
+    font-size: 24px;
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth,
+    },
+    visible: {
+        x: 0,
+    },
+    exit: {
+        x: -window.outerWidth,
+    },
+};
+
+const offset = 6;
+
 function Home() {
     const { data, isLoading } = useQuery<IMoviesResult>(
         ["movies", "nowPlaying"],
         getMovies
     );
-    console.log(data, isLoading);
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data?.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+        }
+    };
+    const toggleLeaving = () => setLeaving((prev) => !prev);
     return (
         <Wrapper>
             {isLoading ? (
@@ -49,6 +99,7 @@ function Home() {
             ) : (
                 <>
                     <Banner
+                        onClick={increaseIndex}
                         bgPhoto={makeImagePath(
                             data?.results[0].backdrop_path || ""
                         )}
@@ -56,6 +107,37 @@ function Home() {
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
+                    <Slider>
+                        <AnimatePresence
+                            initial={false}
+                            onExitComplete={toggleLeaving}
+                        >
+                            <Row
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {data?.results
+                                    .slice(1)
+                                    .slice(
+                                        offset * index,
+                                        offset * index + offset
+                                    )
+                                    .map((movie) => (
+                                        <Box
+                                            key={movie.id}
+                                            bgPhoto={makeImagePath(
+                                                movie.backdrop_path,
+                                                "w500"
+                                            )}
+                                        ></Box>
+                                    ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
         </Wrapper>
